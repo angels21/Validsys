a34a5a008a69f856e4d72809d1d2310184b0db2d
angels21 <62150782+angels21@users.noreply.github.com>
2020-04-10 06:36:51 +0100

    Update Validsys.py

--- Validsys.py
+++ Validsys.py
@@ -1,5 +1,7 @@
 import random
 import string
+import uuid
+
 print('Enter your First Name at least two characters')
 firstName = input()
 while len(firstName) < 2:
@@ -18,6 +20,7 @@ while len(email) < 5:
     print('Enter your email address at least 5 characters')
     email = input()
 
+id = uuid.uuid1()
 
 def randomString(stringLength=5):
     """Generate a random string of fixed length """
@@ -25,21 +28,22 @@ def randomString(stringLength=5):
     return ''.join(random.choice(letters) for i in range(stringLength))
 randomstring = randomString(5)
 print('Do you want to use ' + firstName[0:2] + randomstring + lastName[-2:]+' as password?')
-answer = input('Enter yes or no: ')
-if answer == "yes":
+password = input('Enter yes or no: ')
+if password == "yes":
     password = (firstName[0:2] + randomstring + lastName[-2:])
     print('Your password is saved in the backend press enter to print your saved details')
-    Container = [['First Name', firstName], ['Last Name', lastName], ['Email', email], ['Password', password ]]
-    for row in Container:
-                        print("{: >20} {: >20}".format(*row))
 
-elif answer == "no":
+    Container = [['First Name', firstName], ['Last Name', lastName], ['Email', email], ['Password', password ], ['uniqueid', id.node]]
+    for row in Container:
+        print("{: >20} {: >20}".format(*row))
+
+elif password == "no":
 
     print('Enter your preferred password which must be more than 7 characters')
 password = input()
 while len(password) < 7:
-    print('Enter your password aand it should not be less than 7 characters')
+    print('Enter atleast 7 characters as your password')
     password = input()
-Container = [['First Name', firstName], ['Last Name', lastName], ['Email', email], ['Password', password ]]
+Container = [['First Name', firstName], ['Last Name', lastName], ['Email', email], ['Password', password ], ['uniqueid', id.node]]
 for row in Container:
                         print("{: >20} {: >20}".format(*row))
